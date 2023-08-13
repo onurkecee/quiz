5c1a2340d9e5884cd5ca6e830c89380b4ac5a784
Onur Keçe <onurkecee@gmail.com>
2023-08-13 20:30:09 +0300

    Jetstream kurulumu yapıldı Bootstrap kurulumu yapıldı Jetstream prpfil foto yapılandırıldı Layout yapısı düzenlendi

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+import 'bootstrap/dist/css/bootstrap.min.css';
+window.Alpine = Alpine;
+
+Alpine.plugin(focus);
+
+Alpine.start();
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">Anasayfa</x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="alert alert-danger">Alert Mesajı</div>
+        </div>
+    </div>
+</x-app-layout>
